74cad1b0cb0c8b5bbb74e0e51d03dc60592e01ee
Nikita <97429674+Astaforkin@users.noreply.github.com>
2023-08-15 14:49:13 +0300

    add fix urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,4 +1,6 @@
-from api.views import IngredientViewSet, TagViewSet, UserViewSet
+from api.views import IngredientViewSet, TagViewSet, UserViewSet, RecipeViewSet
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import include, path, re_path
 from rest_framework.routers import DefaultRouter
 
@@ -8,6 +10,7 @@ router = DefaultRouter()
 router.register('users', UserViewSet, 'users')
 router.register('tags', TagViewSet, 'tags')
 router.register('ingredients', IngredientViewSet, 'ingredients')
+router.register('recipes', RecipeViewSet, 'recipes')
 
 urlpatterns = [
     path('', include(router.urls)),
