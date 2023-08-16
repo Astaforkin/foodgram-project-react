dcf47d814cb8bad222d4e59b5ec11a827a4995a3
Nikita Astaforkin <astaforkin90@gmail.com>
2023-08-16 23:24:27 +0300

    fix

--- backend/api/management/commands/load_data.py
+++ backend/api/management/commands/load_data.py
@@ -9,15 +9,8 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         with open('static/data/ingredients.csv', encoding='utf-8-sig') as f:
             reader = csv.reader(f)
-            try:
-                for row in reader:
-                    Ingredient.objects.get_or_create(
-                        name=row[0],
-                        measurement_unit=row[1]
-                    )
-            except Exception as error:
-                print(row)
-                print(
-                        f'Ошибка записи в таблицу модели '
-                        f'{cf["model"].__name__}, '
-                        f'{str(error)}')
+            for row in reader:
+                Ingredient.objects.get_or_create(
+                    name=row[0],
+                    measurement_unit=row[1]
+                )
